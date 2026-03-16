2f3bf5191e3021e30605146dbe4b3fe3490b805a
kcepeda <slenderstalin@hotmail.com>
2026-03-16 13:39:39 -0500

    fix: correccion del dto al actualizar usuario

--- CC.Application/Modules/Identity/Dtos/RegisterRequest.cs
+++ CC.Application/Modules/Identity/Dtos/RegisterRequest.cs
@@ -5,10 +5,10 @@
         public Guid? Id { get; set; }
         public string UserName { get; set; } = default!;
         public string Email { get; set; } = default!;
-        public string Password { get; set; } = default!;
+        public string? Password { get; set; } = default!;
         public string FirstName { get; set; } = default!;
         public string LastName { get; set; } = default!;
-        public string ConfirmPassword { get; set; } = default!;
+        public string? ConfirmPassword { get; set; } = default!;
         public Guid RoleId { get; set; }
     }
 }
